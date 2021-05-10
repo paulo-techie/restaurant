e0555ecd0018995a8c6bc301b09d33057efdd092
Paul Omondi <paulo.techie@gmail.com>
2021-05-10 20:58:33 +0300

    replace double quotes on index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
-import loadPage from './loaders'
-
-loadPage();
-
-let aboutBtn = document.getElementById('aboutBtn');
-let menuBtn = document.getElementById('menuBtn');
-let contactBtn = document.getElementById('contactBtn');
-
-aboutBtn.addEventListener('click', event => { loadTab(event, 'aboutus') });
-menuBtn.addEventListener('click', event => { loadTab(event, 'menu') });
-contactBtn.addEventListener('click', event => { loadTab(event, 'contactus') });
+import loadPage from './loaders';
 
 function loadTab(evt, tabName) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
   // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
+  tabcontent = document.getElementsByClassName('tabcontent');
   for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+    tabcontent[i].style.display = 'none';
   }
 
   // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
+  tablinks = document.getElementsByClassName('tablinks');
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].className = tablinks[i].className.replace(' active', '');
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
+  document.getElementById(tabName).style.display = 'block';
+  evt.currentTarget.className += ' active';
 }
+
+
+loadPage();
+
+const aboutBtn = document.getElementById('aboutBtn');
+const menuBtn = document.getElementById('menuBtn');
+const contactBtn = document.getElementById('contactBtn');
+
+aboutBtn.addEventListener('click', event => { loadTab(event, 'aboutus') });
+menuBtn.addEventListener('click', event => { loadTab(event, 'menu') });
+contactBtn.addEventListener('click', event => { loadTab(event, 'contactus') });
